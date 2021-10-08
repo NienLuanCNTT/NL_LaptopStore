a01293d81b0fa9ca596a13e348f114404a9ffa80
Toi Nguyen <thanhtoi123xyz@gmail.com>
2021-10-08 21:45:48 +0700

    slider 9:45P.M 10/8

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,10 +2,14 @@ import sliderData from 'assets/fake-data/slider'
 import Slider from 'components/Slider'
 import React from 'react'
 
+import Helmet from 'components/Helmet'
+
 
 const Home = () => {
     return (
-        <Slider data={sliderData} />
+        <Helmet title="Trang chủ">
+            <Slider data={sliderData} />
+        </Helmet>
     )
 }
 

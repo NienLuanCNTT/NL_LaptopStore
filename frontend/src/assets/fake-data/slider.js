615ed913fb7d8efb69848a4aa849d21c90caa106
Toi Nguyen <thanhtoi123xyz@gmail.com>
2021-10-08 10:40:55 +0700

    add fake data slider

---
+++ frontend/src/assets/fake-data/slider.js
@@ -0,0 +1,4 @@
+const img1 = require("../images/sliders/slide1.png").default
+const img2 = require("../images/sliders/slide1.png").default
+const img3 = require("../images/sliders/slide1.png").default
+const img4 = require("../images/sliders/slide1.png").default

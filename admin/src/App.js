59302916f918c5b11aa2105f0408f63593864756
Toi Nguyen <thanhtoi123xyz@gmail.com>
2021-12-02 08:26:15 +0700

    2/12

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,6 +4,7 @@ import Home from 'pages/Home/Home';
 import UserList from 'pages/UserList/UserList';
 import './sass/index.scss';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import User from 'pages/User/User';
 
 
 function App() {
@@ -19,6 +20,8 @@ function App() {
           <Route path="/" element={<Home />} />
 
           <Route path="/users" element={<UserList />} />
+
+          <Route path="/user/:id" element={<User />} />
 
 
         </Routes>
